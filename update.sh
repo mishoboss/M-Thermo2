b401f07622851dd02c97ffb8a85ba42ae1388000
Mihail <mishoboss@gmail.com>
2018-01-15 01:01:32 +0200

    create a README

--- update.sh
+++ update.sh
@@ -1,1 +1,1 @@
-./ota.py -l localhost -p 1883 -i bathroom ./esp_firmware/M-Thermo/M-Thermo.ino.d1_mini.bin
+./ota.py -l localhost -p 1883 -i deviceid ./esp_firmware/M-Thermo/M-Thermo.ino.d1_mini.bin
